1511edddc04ce5928232106856e114f53cc63dde
Yo-pic <69393546+Yo-pic@users.noreply.github.com>
2021-07-07 22:29:22 +0900

    Greetings added

--- Dice_Game.py
+++ Dice_Game.py
@@ -1,5 +1,15 @@
 import random
+
+
+def greed():
+    print("What is your name?")
+    greed = input('> ')
+    return greed
+
+
 lists = []
+greed = greed()
+print("Hello, " + greed + "!")
 print("Rolling the dice...")
 for i in range(2):
     lists.append(random.randint(1, 6))
@@ -7,3 +17,4 @@ for i in range(1, 3):
     print("Die {}:".format(i), lists[i - 1])
 
 print("Total Value:", sum(lists))
+

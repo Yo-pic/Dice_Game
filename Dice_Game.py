391d2807f0672f8978d22c46e6c28e10ef737bd0
Yo-pic <69393546+Yo-pic@users.noreply.github.com>
2021-07-07 22:39:50 +0900

    Won/lost message modified

--- Dice_Game.py
+++ Dice_Game.py
@@ -1,5 +1,15 @@
 import random
+
+
+def greed():
+    print("What is your name?")
+    greed = input('> ')
+    return greed
+
+
 lists = []
+greed = greed()
+print("Hello, " + greed + "!")
 print("Rolling the dice...")
 for i in range(2):
     lists.append(random.randint(1, 6))
@@ -7,3 +17,7 @@ for i in range(1, 3):
     print("Die {}:".format(i), lists[i - 1])
 
 print("Total Value:", sum(lists))
+if sum(lists) > 7:
+    print(greed + " won!")
+else:
+    print(greed + " lost!")
